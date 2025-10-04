594f0f8918527142c890ecdfb22bc0464fb1201c
KotoFeelGood72 <kotenkolove@yandex.ru>
2025-10-05 00:18:30 +0300

    Сделал разметку для страницы магазина

---
+++ app/components/card/ProductsCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <NuxtLink :to="link">
+    <div>
+      <img src="@/assets/images/products-card.png" alt="">
+    </div>
+    <div>
+      <span>{{ price }}</span>
+      <p>{{ title }}</p>
+    </div>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+
+export interface ProductsCardProps {
+  title: string;
+  price: string;
+  image?: string;
+  link: string;
+}
+
+const { title, price, image } = defineProps<ProductsCardProps>()
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/pages/stores/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="page">
+    <div class="container">
+      <swiper-container :pagination="{
+        el: '.slider-pagination',
+        clickable: true,
+      }">
+        <swiper-slide v-for="item in 10" :key="item">
+          <div class="bg"><img src="@/assets/images/store.png" alt=""></div>
+          <div>
+            <p>Product</p>
+            <h2>Microsoft office 360</h2>
+            <Button text-color="white" color="primary">Buy</Button>
+          </div>
+        </swiper-slide>
+        <div class="slider-pagination"></div>
+      </swiper-container>
+      <div>
+        <h2>All products</h2>
+        <div>
+          <ProductsCard title="Microsoft office 360" price="100" link="/products/1" v-for="item in 10" :key="item" />
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import Button from '~/shared/ui/Button.vue';
+import ProductsCard from '~/components/card/ProductsCard.vue';
+
+</script>
+
+<style scoped>
+
+</style>
